d722497a5615061fd3cd5d309a9e5c3cf8155f54
Bas Pennings <baspster@gmail.com>
2018-01-14 16:14:25 +0100

    Missing wrapping strategy

--- Conv2D/WrappingStrategy.cs
+++ Conv2D/WrappingStrategy.cs
@@ -3,7 +3,7 @@ namespace Conv2D
     using System;
 
     public class WrappingStrategy<T> : AccumulationStrategy<T>
-        where T: struct, IEquatable<T>, IFormattable
+        where T : struct, IEquatable<T>, IFormattable
     {
         public WrappingStrategy(Accumulator<T> accumulate) : base(accumulate)
         {
@@ -11,7 +11,43 @@ namespace Conv2D
 
         public override T Accumulate(AccumulationState<T> state)
         {
-            throw new NotImplementedException();
+            var kcp = Convolution.GetKernelCenterPoint(state.Kernel);
+            var ksum = Convolution.GetKernelSum(state.Kernel);
+            var acc = default(T);
+
+            for (var rk = -kcp.Item1; rk <= +kcp.Item1; rk++)
+            {
+                for (var ck = -kcp.Item2; ck <= +kcp.Item2; ck++)
+                {
+                    var rt = state.ImageRow + rk;
+                    var ct = state.ImageColumn + ck;
+
+                    if (rt < 0)
+                    {
+                        rt = state.Image.RowCount + rk;
+                    }
+
+                    if (rt >= state.Image.RowCount)
+                    {
+                        rt = rk;
+                    }
+
+                    if (ct < 0)
+                    {
+                        ct = state.Image.ColumnCount + ck;
+                    }
+
+                    if (ct >= state.Image.ColumnCount)
+                    {
+                        ct = ck;
+                    }
+
+                    var w = state.Kernel[kcp.Item1 + rk, kcp.Item2 + ck] / ksum;
+                    acc = this.accumulate(acc, state.Image[rt, ct], w);
+                }
+            }
+
+            return acc;
         }
     }
 }
